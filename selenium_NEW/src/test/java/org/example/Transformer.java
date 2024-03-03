f8d9ea12f7663755ed9247b09a5969434b969b4f
kiranji <k.k.rider97@gmail.com>
2024-03-04 00:44:50 +0530

    Update Transformer.java

--- selenium_NEW/src/test/java/org/example/Transformer.java
+++ selenium_NEW/src/test/java/org/example/Transformer.java
@@ -3,6 +3,7 @@ package org.example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -11,7 +12,7 @@ public class Transformer {
     WebDriver driver;
     @Test()
     public void properties(){
-        driver=new ChromeDriver();
+        driver=new FirefoxDriver();
         driver.get("https://www.google.com/");
         driver.quit();
 
