50d720598d31aa1c6a22851ea6c392d42fb85dbe
kiranji <k.k.rider97@gmail.com>
2024-03-04 01:05:23 +0530

    Update Transformer.java

--- selenium_NEW/src/test/java/org/example/Transformer.java
+++ selenium_NEW/src/test/java/org/example/Transformer.java
@@ -4,23 +4,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.net.URL;
+
 public class Transformer {
 
-    // WebDriver driver;
+    WebDriver driver;
     @Test()
     public void properties(){
-        URL url=new URL("http://localhost:4444/"); 
+//        driver=new FirefoxDriver();
+        URL url=new URL("http://localhost:4444/");
 
- 
 
-DesiredCapabilities cap=new DesiredCapabilities(); 
 
-cap.setBrowserName("chrome"); 
+        DesiredCapabilities cap=new DesiredCapabilities();
 
-RemoteWebDriver driver=new RemoteWebDriver(url, cap); 
+        cap.setBrowserName("chrome");
+
+        RemoteWebDriver driver=new RemoteWebDriver(url, cap);
         driver.get("https://www.google.com/");
         driver.quit();
 
