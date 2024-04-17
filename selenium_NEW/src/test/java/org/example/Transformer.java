4c3d7e1ffdc50d5287a30dee63c0221577c12e76
kiranji <k.k.rider97@gmail.com>
2024-04-17 12:48:11 +0530

    Update Transformer.java

--- selenium_NEW/src/test/java/org/example/Transformer.java
+++ selenium_NEW/src/test/java/org/example/Transformer.java
@@ -19,7 +19,7 @@ public class Transformer {
     @Test()
     public void properties() throws MalformedURLException {
 //        driver=new FirefoxDriver();
-        URL url=new URL("http://100.27.27.68:4444/");
+        URL url=new URL("https://127.0.0.1:4444/");
 
 
 
